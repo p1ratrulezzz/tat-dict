fb5f330f4d35d38e5b15f31205191245ac489e25
p1ratrulezzz <git@p1ratrulezzz.me>
2020-06-15 17:40:03 +0300

    Build

--- scripts/rebuild_from_index.js
+++ scripts/rebuild_from_index.js
@@ -1,11 +1,56 @@
 'use strict';
 
 const fs = require('fs');
+const sortObject = require('sort-object-keys');
 
-let indexedPath = __dirname + '../data/index';
+Object.prototype.sortAsc = function() {
+    return Object.assign({}, sortObject(this));
+};
 
+let indexedPath = __dirname + '/../data/index';
 function rebuildIndexCb(err, files) {
-    let a = 1;
+    if (err) {
+        throw err;
+    }
+
+    let jsonContent = {};
+    for (let i=0; i < files.length; i++) {
+        let filename = files[i];
+        let filepath = indexedPath + '/' + files[i];
+
+        if (filename === '.' || filename === '..' || filename === '___broken.min.json') {
+            continue;
+        }
+
+        let jsonData = JSON.parse(fs.readFileSync(filepath));
+        jsonContent = Object.assign(jsonData, jsonContent);
+    }
+
+    let jsonKeyed = {};
+    jsonContent = jsonContent.sortAsc();
+    let ref = 1;
+    for (let p in jsonContent) {
+        if (!jsonContent.hasOwnProperty(p)) {
+            continue;
+        }
+
+        let word = p;
+        let definitions = jsonContent[word];
+        jsonKeyed[word] = {
+            'id': ref,
+            'word': word,
+            'definitions': definitions,
+        };
+
+        ref++;
+    }
+
+    dataRead(jsonKeyed);
 }
 
-fs.readDir(indexedPath, rebuildIndexCb());
+function dataRead(data) {
+    let a = 1;
+    fs.writeFileSync(indexedPath + '/../build.json', JSON.stringify(data, null, 4));
+}
+
+fs.readdir(indexedPath, rebuildIndexCb);
