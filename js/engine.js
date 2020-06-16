3c2d4edde3306bb1acbd67bd71dfb48d4786df12
P1ratRuleZZZ <p1ratrulezzz@users.noreply.github.com>
2020-06-17 00:39:13 +0300

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,5 +1,5 @@
 (function($) {
-    let indexLoadedPromise = new Promise((resolve, reject) => {
+    let indexLoadedPromise = new Promise(function(resolve, reject) {
         (function pakoTest() {
             if (window.pako != null && pako.inflate != null && window.jsonHelper != null) {
                 return resolve();
@@ -7,8 +7,8 @@
 
             setTimeout(pakoTest, 30);
         }) ();
-    }).then(() => {
-        return new Promise((resolve, reject) => {
+    }).then(function() {
+        return new Promise(function(resolve, reject) {
             jsonHelper.loadJSON('/data/index.lunr.min.json.gz', function(response) {
                 let data = JSON.parse(pako.inflate(response, { to: 'string' }));
                 resolve(data);
@@ -20,7 +20,7 @@
         $ = window.jQuery;
 
         // The page is fully loaded.
-        indexLoadedPromise.then((data) => {
+        indexLoadedPromise.then(function(data) {
             let lunrIndex = lunr.Index.load(data);
             indexLoaded(lunrIndex);
         });
@@ -56,7 +56,7 @@
                 fields: [],
             }, params);
 
-            let results = lunrIndex.query((q) => {
+            let results = lunrIndex.query(function(q) {
                 let wildcard = lunr.Query.wildcard.NONE;
                 if (params.wildcard_left) {
                     wildcard |= lunr.Query.wildcard.LEADING;
@@ -73,25 +73,25 @@
                     lunrOptions.fields = params.fields;
                 }
 
-                querystring.toLocaleLowerCase().split(' ').forEach((word) => {
+                querystring.toLocaleLowerCase().split(' ').forEach(function(word) {
                     q.term(word.trim(), lunrOptions);
                 });
             });
 
             let resultsFound = [];
-            let searchPromise = new Promise((resolve, reject) => {
+            let searchPromise = new Promise(function(resolve, reject) {
                 resolve(resultsFound);
             });
 
-            results.forEach((result) => {
+            results.forEach(function(result) {
                 let ref = result.ref.split(':');
                 let letter = ref[0];
                 let id = ref[1];
-                searchPromise = searchPromise.then(() => {
+                searchPromise = searchPromise.then(function() {
                     if (wordsData[letter] == null) {
-                        return new Promise((resolve, reject) => {
+                        return new Promise(function(resolve, reject) {
                             let jsonFile = '/data/index.gz/' + letter + '.min.json.gz';
-                            jsonHelper.loadJSON(jsonFile, (response) => {
+                            jsonHelper.loadJSON(jsonFile, function(response) {
                                 let _data = JSON.parse(pako.inflate(response, {to: 'string'}));
                                 wordsData[letter] = _data;
                                 resultsFound.push(wordsData[letter][id]);
@@ -116,7 +116,7 @@
             let onlyWordsSearch = $('#only-word-search').prop('checked');
             let wildcardSearch = $('#wildcard-search').prop('checked');
 
-            new Promise((resolve) => {
+            new Promise(function(resolve) {
                 disableSearch();
                 location.hash = '#' + $searchBox.val() + '?only-word-search=' + String(onlyWordsSearch ? 1 : 0) + '&wildcard-search=' + String(wildcardSearch ? 1 : 0);
 
@@ -124,12 +124,12 @@
                     // Small timeout to prevent browser stuck.
                     resolve();
                 }, 30);
-            }).then(() => {
+            }).then(function() {
                 searchAndGetWord(queryString, {
                     'wildcard_left': wildcardSearch,
                     'wildcard_right': wildcardSearch,
                     'fields': onlyWordsSearch ? ['word', 'slug'] : [],
-                }).then((resultsFound) => {
+                }).then(function(resultsFound) {
                     let templateHtml = $('.templates-wrapper').html();
                     let $searchResultsWrapper = $('.search-results');
                     $searchResultsWrapper.html('');
@@ -141,7 +141,7 @@
                         $searchResultsWrapper.append($item);
                     }
                     else {
-                        resultsFound.forEach((result) => {
+                        resultsFound.forEach(function(result) {
                             let $item = $(templateHtml);
                             $item.find('.word').html(result.word);
                             $item.find('.definition').html(result.definition);
@@ -156,7 +156,7 @@
         }
 
         $searchButton.on('click', doSearch);
-        $searchBox.on('keyup', (e) => {
+        $searchBox.on('keyup', function(e) {
             if (e.keyCode === 13) {
                 doSearch();
             }
