2578082d2bfed5409252a88d1b887dcc193fd044
Rtxrulez <rtx@rtx.ru>
2022-10-27 15:20:08 +0600

    minimize js

--- js/json_helper.js
+++ js/json_helper.js
@@ -1,27 +1,30 @@
 (function () {
-    let jsonHelper = function() {
-        this.loadJSON = function (jsonFile, callback) {
-            var xobj = new XMLHttpRequest();
-            xobj.overrideMimeType("application/json");
-            xobj.open('GET', jsonFile, true);
-            // xobj.setRequestHeader('Content-Encoding','gzip');
-            xobj.onerror = function (e) {
-                alert("Error " + e.target.status + " occurred while receiving the document. Try to refresh the page");
-            };
+  let jsonHelper = function () {
+    this.loadJSON = function (jsonFile, callback) {
+      var xobj = new XMLHttpRequest();
+      xobj.overrideMimeType('application/json');
+      xobj.open('GET', jsonFile, true);
+      // xobj.setRequestHeader('Content-Encoding','gzip');
+      xobj.onerror = function (e) {
+        alert(
+          'Error ' +
+            e.target.status +
+            ' occurred while receiving the document. Try to refresh the page',
+        );
+      };
 
-            xobj.onreadystatechange = function () {
-                if (xobj.readyState == 4 && xobj.status == 200) {
+      xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == 200) {
+          // .open will NOT return a value but simply returns undefined in async mode so use a callback
+          callback(xobj.responseText);
+        }
+      };
 
-                    // .open will NOT return a value but simply returns undefined in async mode so use a callback
-                    callback(xobj.responseText);
-                }
-            };
-
-            xobj.send(null);
-        };
-
-        return this;
+      xobj.send(null);
     };
 
-    window.jsonHelper = new jsonHelper();
+    return this;
+  };
+
+  window.jsonHelper = new jsonHelper();
 })();
